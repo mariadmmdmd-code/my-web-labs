c9b5fc0e13892e589d329b0ccf09fd0524dc9d4e
maria programmer <maria.coc.1023@gmail.com>
2026-03-24 20:37:43 +0000

    Auto-save: Tue Mar 24 08:37:43 PM UTC 2026

--- my_gorg_website/pages/admin.py
+++ my_gorg_website/pages/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    
+    list_filter = ('is_staff', 'is_active')
+    
+    # Поля, которые показывать при редактировании
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('bio',)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
